6a8ff3eef41ba636dec682e387c74aa5917c64ad
happy-quokka98 <307491385+happy-quokka98@users.noreply.github.com>
2026-09-26 17:36:13 +0400

    created vite files

---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.figma.com/design/U6ltucuISkyseUqHk6OuQU/BankDash---Dashboard-UI-Kit---Admin-Template-Dashboard---Admin-Dashboard--Community-?node-id=0-1&p=f&t=woWUN6eYcPApCZ7C-0
+
---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+        <Route path="/" element={< />}/>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
